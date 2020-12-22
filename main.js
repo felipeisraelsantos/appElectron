39ab3a8f88cebf4582a430de9a43ee226af14306
felipe_dev2 <desenvolvedor2@contabexpress.com.br>
2020-12-22 11:51:41 -0300

    projeto electron app

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
